a8a685544feb03e2333bc150f7ce47dd2e599b85
Bhushan Mahale <bhushanm@synnefatech.com>
2026-04-17 15:21:47 +0530

    config updated

--- config/config.js
+++ config/config.js
@@ -21,7 +21,7 @@ module.exports = {
   production: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME_PROD || 'database_production',
+    database: process.env.DB_NAME || 'database_production',
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     dialect: process.env.DB_DIALECT || 'mysql',
